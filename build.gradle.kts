4bb5b6c3da23e8be00a572578fd191a05f07956b
Helvetica Volubi <suisuroru@blue-millennium.fun>
2026-07-12 16:22:40 +0800

    [ci skip] fix ci for update

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 paperweight {
     filterPatches = false
     upstreams.register("luminol") {
-        repo = github("LuminolMC", "Luminol")
+        repo = github("LuminolCustomArchive", "Luminol")
         ref = providers.gradleProperty("luminolRef")
 
         println("Upstream commit ref: " + ref.get())
